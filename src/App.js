00d775904e54b580dbfc27ccf7b1fd67a2e474ee
Egor Molchan <90382636+egobars@users.noreply.github.com>
2023-06-06 09:00:00 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <span>a</span>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.login = this.login.bind(this);
+    }
+
+    async login() {
+        /*let data = {
+            email: username,
+            password: password
+        };*/
+
+        const config = {
+            url: '/log',
+            method: 'post',
+            headers: {
+                'content-type': 'application/json',
+            },
+            data: {
+                'email': 'aaa',
+                'password': 'aaa'
+            },
+            withCredentials: true,
+        }
+
+        await axios(config).then(res => {
+            console.log(res);
+            console.log(res.headers["set-cookie"]);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+
+    render() {
+        this.login();
+        
+        return (
+            <span>b</span>
+        )
+    }
 }
 
 export default App;
